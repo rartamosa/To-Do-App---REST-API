18260efc683146837eed7fe235df7cb9b1a6de7a
Marta Rosa <marta.r.93@gmail.com>
2023-01-16 18:53:02 +0100

    Added get and post requests for tasks

--- server.js
+++ server.js
@@ -15,7 +15,150 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const TaskSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: [true, "Task has to have a title"],
+  },
+  description: {
+    type: String,
+    required: [true, "Task has to have a description"],
+  },
+  link: {
+    type: String,
+    required: [true, "Task has to have a link attached"],
+  },
+  tags: {
+    type: Array,
+    required: [true, "Task has to have tag(s) attached"],
+  },
+  dueDate: {
+    type: Date,
+    default: new Date().toDateString(),
+  },
+  assignee: {
+    type: "String",
+    required: [true, "Task has to have an assignee"],
+  },
+  column: {
+    type: String,
+    required: [true, "Task has to be assigned to a specific column"],
+    // default: ,
+  },
+  comments: {
+    type: Array,
+    required: false,
+  },
+});
+
+const Task = mongoose.model("Task", TaskSchema);
+// __________________________________________________________________
+
 // Write your endpoints below:
+// TASKS
+app.get("/tasks", async (req, res) => {
+  const { assignee, column, tags, page, perPage } = req.query;
+
+  const query = {
+    ...(assignee && { title: new RegExp(assignee, "i") }),
+    ...(column && { column: new RegExp(column, "i") }),
+    ...(tags && { tags: new RegExp(tags, "i") }),
+  };
+
+  const PageParam = page || 1;
+  const perPageParam = perPage || 20;
+
+  try {
+    const tasks = await Task.aggregate([
+      { $match: query },
+      { $skip: (Number(PageParam) - 1) * Number(perPageParam) },
+      { $limit: +perPageParam },
+      // {$lookup: {
+      //   from: "",
+      //   localField: "",
+      //   foreignField: "_id",
+      //   as: "",
+      // }},
+    ]);
+    res.status(200).json({
+      data: tasks,
+      success: true,
+    });
+  } catch (error) {
+    res.status(400).json({
+      data: error,
+      success: false,
+    });
+  }
+});
+
+app.post("/tasks", async (req, res) => {
+  const {
+    title,
+    description,
+    link,
+    tags,
+    dueDate,
+    assignee,
+    column,
+    comments,
+  } = req.body;
+
+  try {
+    const task = await new Task({
+      title,
+      description,
+      link,
+      tags,
+      dueDate,
+      assignee,
+      column,
+      comments,
+    }).save();
+    res.status(201).json({
+      data: task,
+      success: true,
+    });
+  } catch (error) {
+    res.status(400).json({
+      data: error,
+      success: false,
+    });
+  }
+});
+
+app.put("tasks/:taskId", async (req, res) => {
+  const { taskId } = req.params;
+  const {
+    title,
+    description,
+    link,
+    tags,
+    dueDate,
+    assignee,
+    column,
+    comments,
+  } = req.body;
+  console.log(taskId);
+
+  try {
+    const task = await Task.findByIdAndUpdate(
+      taskId,
+      { title, description, link, tags, dueDate, assignee, column, comments },
+      { new: true, runValidators: true }
+    );
+    res.status(201).json({
+      data: task,
+      success: true,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({
+      data: error,
+      success: false,
+    });
+  }
+});
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
